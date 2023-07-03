5d701b0d619b69f008f25ce8494524c051c013da
Wael Sadek <wael.sadek@gmail.com>
2023-07-03 16:37:16 +0300

    app to drill access

--- securitygroup.tf
+++ securitygroup.tf
@@ -80,9 +80,16 @@ module "drill_sg" {
       to_port                  = 31012
       protocol                 = "tcp"
       source_security_group_id = "${module.zookeeper_sg.security_group_id}"
+    },
+   {
+      from_port                = 31010
+      to_port                  = 31012
+      protocol                 = "tcp"
+      source_security_group_id = "${module.app_sg.security_group_id}"
     }
+ 
   ]
-  number_of_computed_ingress_with_source_security_group_id = 2
+  number_of_computed_ingress_with_source_security_group_id = 3
 
   egress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules       = ["all-all"]
